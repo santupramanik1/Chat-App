7112490b56f7a2743bbb4d65dc7ff25834bc60d8
Santu pramanik <santu700141@gmail.com>
2025-07-01 00:46:00 +0530

    Design Signup and OTP Verify Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import {HomePage} from "./pages/HomePage"
 import { LoginPage } from './pages/LoginPage'
 import {ProfilePage} from "./pages/ProfilePage"
+import { EmailVerifyPage } from './pages/EmailVerifyPage'
  const App = () => {
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-cover bg-center w-full h-screen ">
@@ -10,6 +11,7 @@ import {ProfilePage} from "./pages/ProfilePage"
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/profile' element={<ProfilePage />}/>
+        <Route path='/email-verify' element={<EmailVerifyPage/>}></Route>
       </Routes>
     </div>
   )
